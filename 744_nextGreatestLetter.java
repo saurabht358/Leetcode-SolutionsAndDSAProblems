59609f44b01921eedc4135ab50c9e176070da233
saurabht358 <91771423+saurabht358@users.noreply.github.com>
2024-04-10 12:24:58 +0530

    Update 744_nextGreatestLetter.java

--- 744_nextGreatestLetter.java
+++ 744_nextGreatestLetter.java
@@ -1,3 +1,4 @@
+//Soulution No.1
 class Solution {
     public char nextGreatestLetter(char[] arr, char target) {
         int l=0,h=arr.length-1;
@@ -15,5 +16,23 @@ class Solution {
 			else h=mid-1;
 		}
 		return arr[l];
+    }
+}
+//Solution No.2
+class Solution {
+    public char nextGreatestLetter(char[] arr, char target) {
+        int l=0,h=arr.length-1;
+		if(arr[arr.length-1]<target) return arr[0];
+		if(arr[0]>target) return arr[0];
+		int mid=0;
+        while(l<=h){
+		mid = l+(h-l)/2;
+		if(arr[mid]<=target){
+                	if (mid>=arr.length-1) return arr[0];
+                	l=mid+1;
+            	}
+		else h=mid-1;
+	}
+	return arr[l];
     }
 }
