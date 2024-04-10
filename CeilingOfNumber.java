e4ca4030572f722f293ca8d83cf3eeb22b33f946
saurabht358 <91771423+saurabht358@users.noreply.github.com>
2024-04-10 11:42:28 +0530

    Add files via upload

---
+++ CeilingOfNumber.java
@@ -0,0 +1,19 @@
+Class CeilingOfNumber{
+	public static void main(String []args){
+		int []arr= {2,4,5,8,14,16,19};
+		int target=0;
+		System.out.println(ceilofArr(arr,target));
+	}
+	public static int ceilofArr(int arr[],int target){
+		int l=0,h=arr.length-1;
+		if(arr[arr.length-1]<target) return -1;
+		if(arr[0]>target) return arr[0];
+		while(l<=h){
+			int mid = l+(h-l)/2;
+			if(arr[mid]==target)return arr[mid];
+			else if(arr[mid]<target)l=mid+1;
+			else h=mid-1;
+		}
+		return arr[l];
+	}
+}
