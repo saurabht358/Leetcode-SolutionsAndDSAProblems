fd8aac53797ae9baa0f7b4409ea8536be7b8a0e0
saurabht358 <91771423+saurabht358@users.noreply.github.com>
2024-04-10 17:28:06 +0530

    InfiniteArray.java

--- InfiniteArray.java
+++ InfiniteArray.java
@@ -5,14 +5,14 @@
         int arr[]={1,3,4,5,7,9,15,16,18,21,25,29,35,65,124,128,135,138,148,168,175,185,188,189,495,500,501,502,507,509,554,557,559,584};
         int start=0,end=0;
         while(true){
-            if(arr[l] <= target && arr[h] >= target){
+            if( arr[h] >= target){
                 start = l;
                 end = h;
                 break;
             }
-            int temp = 2 *l;
+            int temp = 2 * (h-l+1);
             l = h+1;
-            h = l + temp;
+            h = h + temp;
         }
         int mid=-1;
         while(start<=end){
