4bc5e31362e2f20dc759b0fc13400a5a44327fb4
saurabht358 <91771423+saurabht358@users.noreply.github.com>
2024-04-11 14:28:38 +0530

    Add second solution 162FindPeakElement.java

--- 162FindPeakElement.java
+++ 162FindPeakElement.java
@@ -1,3 +1,4 @@
+//Solution No.1
 class Solution {
     public int findPeakElement(int[] arr) {
         
@@ -13,5 +14,36 @@ class Solution {
             }
         }
         return l;  
+    }
+}
+
+//Solution No.2
+class Solution {
+    public int findPeakElement(int[] arr) {
+        
+        int max = Integer.MIN_VALUE,index=-1;  
+        int l=0,h=arr.length-1;
+
+        while(l<=h){
+            int mid= l+(h-l)/2;
+            if(mid==arr.length-1){
+                index=mid;
+                break;
+            }
+            if(arr[mid]>arr[mid+1]){
+                if(arr[mid]>max){
+                    max= arr[mid];
+                    index = mid;
+                }
+                h= mid-1;
+            }else{
+                if(arr[mid]>max){
+                    max= arr[mid];
+                    index = mid;
+                }
+                l= mid+1;
+            }
+        }
+        return index;  
     }
 }
