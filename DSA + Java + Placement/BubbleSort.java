12b3b3c65162c498a61d1a71123678f35582654f
saurabht358 <91771423+saurabht358@users.noreply.github.com>
2024-04-22 22:31:50 +0530

    Update BubbleSort.java

--- DSA + Java + Placement/BubbleSort.java
+++ DSA + Java + Placement/BubbleSort.java
@@ -3,13 +3,18 @@ class JavaDSA{
     public static void main(String[] args) {
         int []arr={55,8,3,1,4,-4};
         for(int i=0;i<arr.length;i++){
+            boolean isSorted = true;
             for(int j=0;j<arr.length-i-1;j++){
+                
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    isSorted = false;
                 }
             }
+            if(isSorted) break;
+            
         }
         System.out.println(Arrays.toString(arr));
     }
