5c27f785615eefa7fad966d9a74f57437472afcb
saurabht358 <91771423+saurabht358@users.noreply.github.com>
2024-05-02 18:05:21 +0530

    Update Pattern Printing.java

--- DSA + Java + Placement/Pattern Printing.java
+++ DSA + Java + Placement/Pattern Printing.java
@@ -25,22 +25,26 @@
             for(int j=0;j<n;j++){
                 if(j<=i)
                     System.out.print("*");
+                else
+                    System.out.print(" ");
             }
             System.out.println();
         }
 
 /*
-*****
-****
-***
-**
+* * * * *
+* * * *
+* * *
+* *
 *
 */
 
           for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(j>=i)
-                    System.out.print("*");
+                if(j<=n-i-1)
+                    System.out.print("* ");
+                else
+                    System.out.print(" ");
             }
             System.out.println();
         }
@@ -55,36 +59,63 @@
             for(int j=0;j<n;j++){
                 if(j<=i)
                     System.out.print(j+1 +" ");
+                else
+                    System.out.print(" ");
             }
             System.out.println();
         }
 /*
 *
-**
-***
-****
-*****
-****
-***
-**
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
 *
 */
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(j<=i)
-                    System.out.print("*");
-            }
-            System.out.println();
-        }
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-1;j++){
-                if(j>=i)
-                    System.out.print("*");
+        for(int i=1;i<=2*n-1;i++){
+            int c = i <= n ? i: 2* n-i;
+            for(int j=1;j<=c;j++){
+                System.out.print("* ");
             }
             System.out.println();
         }
 
+/*
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
 
-          
+*/
+        for(int i=1;i<=2*n-1;i++){
+            int spaces =i<=n ? n-i:i-n;
+            for(int j=1;j <=spaces;j++){
+                System.out.print(" ");
+            }
+            
+            int c = i <= n ? i: 2* n-i;
+            for(int j=1;j<=c;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }  
         
+
+
+ int n=5;
+          for(int i=0;i<=2*n;i++){
+            for(int j=0;j<=2*n;j++){
+                int val = Math.min(i,Math.min(j,Math.min(2*n-i,2*n-j)));
+                System.out.print(val);
+            }
+            System.out.println();
+        }
