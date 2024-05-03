a52168c8f15a0dc5a03803f05b1ecd7b9812f6f7
saurabht358 <91771423+saurabht358@users.noreply.github.com>
2024-05-03 15:43:29 +0530

    Update Pattern Printing.java

--- DSA + Java + Placement/Pattern Printing.java
+++ DSA + Java + Placement/Pattern Printing.java
@@ -266,3 +266,110 @@ static void pattern13(int n){
             System.out.println();
         }
 }
+/*
+    *
+   * *
+  *   *
+ *     *
+*********
+*/
+
+static void pattern14(int n){
+        for(int i=1;i<=n;i++){
+            if(i==1){
+                for(int j=1;j<=n-1;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            else if(i==n){
+                for(int j=1;j<=2*n -1;j++){
+                    System.out.print("*");
+                }
+            }
+            else{
+                for(int j=1;j<=n-i;j++)
+                    System.out.print(" ");
+                System.out.print("*");
+                for(int j=1;j<=(i-1)*2-1;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+}
+
+/*
+*************
+ *         *
+  *       *
+   *     *
+    *   *
+     * *
+      *
+*/
+
+static void pattern15(int n){
+        for(int i=1;i<=n;i++){
+            if(i==n){
+                for(int j=1;j<=n-1;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            else if(i==1){
+                for(int j=1;j<=2*n -1;j++){
+                    System.out.print("*");
+                }
+            }
+            else{
+                for(int j=1;j<=i-1;j++)
+                    System.out.print(" ");
+                System.out.print("*");
+                for(int j=1;j<=2 * n - 2*i-1;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+}
+/*
+      *
+     * *
+    *   *
+   *     *
+  *       *
+ *         *
+*           *
+*           *
+ *         *
+  *       *
+   *     *
+    *   *
+     * *
+      *
+*/
+static void pattern16(int n){
+        for(int i=1;i<=n*2;i++){
+            if(i== 2* n || i==1){
+                for(int j=1;j<=n-1;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            else{
+                int c= i<=n ? n-i:i-n-1;
+                for(int j=1;j<=c;j++)
+                    System.out.print(" ");
+                System.out.print("*");
+                c= i<=n ? 2*(i-1)-1: 2*n -2*(i-n)-1;
+                for(int j=1;j<=c;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+}
