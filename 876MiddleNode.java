27df4224e91cfe95f2a9435e7ffdc643d8274f46
saurabht358 <91771423+saurabht358@users.noreply.github.com>
2024-07-30 17:24:22 +0530

    Writing Comment 876MiddleNode.java

--- 876MiddleNode.java
+++ 876MiddleNode.java
@@ -2,7 +2,7 @@ class Solution {
     public ListNode middleNode(ListNode head) {
         if(head.next==null) return head;
         ListNode slow = head,fast=head; // use fast and slow concept
-        while(fast!=null && fast.next!=null){ when fast  is null that time slow at the middle of list
+        while(fast!=null && fast.next!=null){   // when fast  is null that time slow at the middle of list
             slow = slow.next;  // slow is moving one step at a time
             fast = fast.next.next;  // fast is moving two step at a time || moving double than slow
         }
