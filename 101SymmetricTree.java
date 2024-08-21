d442f06071e77e12da6b16c40e8d1472b72062ef
saurabht358 <91771423+saurabht358@users.noreply.github.com>
2024-08-22 00:25:41 +0530

    Update 101SymmetricTree.java

--- 101SymmetricTree.java
+++ 101SymmetricTree.java
@@ -1,3 +1,4 @@
+//Solution No. 1
 class Solution {
     public boolean isSymmetric(TreeNode root) {
          return isSame(root.left,root.right);
@@ -17,4 +18,37 @@ class Solution {
         return isSame(node1.right,node2.left);
     }
     
+}
+
+
+//Soulution No 2:
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+        while(!queue.isEmpty()){
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+
+            if(left==null && right==null){
+                continue;
+            }
+            if(left==null || right==null){
+                return false;
+            }
+            if(left.val!=right.val){
+                return false;
+            }
+
+            queue.offer(left.left);
+            queue.offer(right.right);
+            queue.offer(left.right);
+            queue.offer(right.left);
+            
+            
+        }
+        return true;
+    }
 }
