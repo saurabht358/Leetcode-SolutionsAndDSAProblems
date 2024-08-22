273b245eabdfba7bc9599a2a85eb7c6c1b12d3bc
saurabht358 <91771423+saurabht358@users.noreply.github.com>
2024-08-22 23:37:08 +0530

    Update 114FlattenBinaryTreeToLL.java

--- 114FlattenBinaryTreeToLL.java
+++ 114FlattenBinaryTreeToLL.java
@@ -31,3 +31,42 @@ class Solution {
     }
 
 }
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode current = root;
+        if(root==null){
+            return;
+        }
+        while(current!=null){
+            if(current.left!=null){
+                TreeNode temp = current.left;
+                while(temp.right!=null){
+                    temp = temp.right;
+                }
+
+                temp.right = current.right;
+                current.right = current.left;
+                current.left = null;
+            }
+            current =  current.right;
+        }
+    }
+}
