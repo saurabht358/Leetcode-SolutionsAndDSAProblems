865f96ca2ad5212a37d5d581e13a01a02707788b
saurabht358 <91771423+saurabht358@users.noreply.github.com>
2024-08-29 15:23:22 +0530

    Update and rename 124BinaryTreeMaximum.java to 124BinaryTreeMaximumPathSum.java

--- 124BinaryTreeMaximumPathSum.java
+++ 124BinaryTreeMaximumPathSum.java
@@ -1,14 +1,10 @@
  
 class Solution {
     public int maxPathSum(TreeNode root) {
-        if(root!=null){
-            maxSum=root.val;
-        }
-        helper(root);
-    
-        return maxSum;
+         helper(root);
+         return maxSum;
     }
-    int maxSum =0;
+    int maxSum =Integer.MIN_VALUE;
     int helper(TreeNode node){
         if(node==null){
             return 0;
@@ -18,17 +14,12 @@ class Solution {
             maxSum = Math.max(node.val,maxSum);
             return node.val;
         }
-
         int left= helper(node.left);
-        
         int right = helper(node.right);
 
         if(left<0) left = 0;
         if(right<0) right = 0;
-        
         maxSum = Math.max(left+right+node.val,maxSum);
-         
-
         return Math.max(left+node.val,right+node.val);
     }
 }
