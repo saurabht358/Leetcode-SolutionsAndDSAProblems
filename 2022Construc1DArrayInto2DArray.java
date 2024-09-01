eccb029d789b750d2df4c82a0a1629cb9cc19f0e
saurabht358 <91771423+saurabht358@users.noreply.github.com>
2024-09-01 15:46:35 +0530

    Create 2022Construc1DArrayInto2DArray.java

---
+++ 2022Construc1DArrayInto2DArray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        
+        if(m*n != original.length) return new int[0][0];
+        int[][] ans = new int[m][n];
+        for(int i=0;i<m;i++){
+            ans[i] = Arrays.copyOfRange(original,i*n,(i*n)+n);
+        }
+        return ans;
+    } 
+}
